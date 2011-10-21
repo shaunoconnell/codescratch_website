b1044dc8740f4b79a0337e5e742cd8835edd1c3e
shaun <shaunoconnell@gmail.com>
2011-10-20 22:17:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codescratchsite::Application.config.secret_token = '885f746696dc03073495e2a4e51bbfe7093da1ebe546f8e80a7ee2a1ba2e8aa806b69b04f92253ec0e51cbed742988dce22fe2ab5dd80007cb8b0c693bb8ff52'
